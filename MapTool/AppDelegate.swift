3637931ebf218390842827465a10e4418cad774b
Pascale Beaulac <pascale@prospects.com>
2018-12-27 12:10:15 -0500

    Initial Commit

---
+++ MapTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MapTool
+//
+//  Created by Pascale Beaulac on 2018-12-27.
+//  Copyright © 2018 Prospects. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MapTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MapTool
+//
+//  Created by Pascale Beaulac on 2018-12-27.
+//  Copyright © 2018 Prospects. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
